56b21e12218a343ebfc1a9820fe3a8f4161e46a5
LingQi000809 <lqi@vassar.edu>
2020-04-06 14:54:10 -0400

    milestone1 completed

--- Milestone 1/milestone1_code/milestone1_code.ino
+++ Milestone 1/milestone1_code/milestone1_code.ino
@@ -1,3 +1,9 @@
+#include <ZumoShield.h>
+
+ZumoMotors motors;
+// speed: 0 (stop) -400 (full)
+int INITIAL_SPEED = 100;
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -5,5 +11,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  motors.setLeftSpeed(INITIAL_SPEED);
+  motors.setRightSpeed(INITIAL_SPEED);
+  delay(4000);
+  INITIAL_SPEED = 0;
 }
