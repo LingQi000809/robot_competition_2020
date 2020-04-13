824de2ed86b24746c3a24e0b23ca94cc2c9fadc9
LingQi000809 <lqi@vassar.edu>
2020-04-13 15:38:45 -0400

    updated code2

--- Milestone 2/milestone2_code/milestone2_code.ino
+++ Milestone 2/milestone2_code/milestone2_code.ino
@@ -14,37 +14,62 @@ int RIGHT_SPEED;
 
 void setup() {
   // put your setup code here, to run once:
+  buzzer.play(">g32>>c32");
   
   // Initialize the sensors
   sensors.init();
 
-  // calibration
+  // calibration by spinning
+  delay(1000);
+  int CALIBRATION_SPEED = 100;
+  for (int t = 0; t < 80; t++){
+    // 0-10s turn clockwise; 10-30s turn anti-clockwise; 30-50s turn clockwise; 50-70s turn anti-clockwise; 70-80 turn clockwise back to front
+    if ((t <= 10) || (t > 30 && t <= 50) || (t>70)){
+      motors.setSpeeds(CALIBRATION_SPEED, -CALIBRATION_SPEED);
+    }
+    else{
+      motors.setSpeeds(-CALIBRATION_SPEED, CALIBRATION_SPEED);
+    }
+    sensors.calibrate();
+    delay(20);
+  }
+
+//  // calibration by hand
+//  for (int t =  0; t<100; t++){
+//    sensors.calibrate();
+//  }
   
- 
-  // wait for the user to press the button
-  // button.waitForButton();
+  buzzer.play(">g32>>c32");
+  motors.setSpeeds(0,0);
+  // Wait for the user button to be pressed and released
+  button.waitForButton();
+  buzzer.play("L16 cegec4");
+  while(buzzer.isPlaying());
 }
 
 void loop() {
+  
   linePos = sensors.readLine(sensorValues);  
   // 0 - directly below sensor 0 
   // 1 - directly below sensor 1
   // 5 - 5000
+  Serial.println(linePos);
 
-  if (linePos == 2500) {
+  if (linePos > 3000) {
+    // anti-clockwise
+    LEFT_SPEED = NORMAL_SPEED;
+    RIGHT_SPEED = 0;
+  }
+  else if (linePos < 2000){
+    // clockwise
+    LEFT_SPEED = 0;
+    RIGHT_SPEED = NORMAL_SPEED;
+  }
+  else{
     LEFT_SPEED = NORMAL_SPEED;
     RIGHT_SPEED = NORMAL_SPEED;
   }
-  else if (linePos < 2500){
-    LEFT_SPEED 
-  }
-  }
+
+  motors.setSpeeds(LEFT_SPEED, RIGHT_SPEED);
+  //delay(100);
 }
-  
-
-
-  
-  // put your main code here, to run repeatedly:
-  motors.setSpeeds(SPEED, SPEED);
-  delay(10000);
-  SPEED = 0;
