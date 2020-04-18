c2555ebbb9d181ed867ad8e9ef4592e24050eb2c
LingQi000809 <lqi@vassar.edu>
2020-04-18 12:50:43 -0400

    finished milestone2 code

---
+++ Milestone 2/milestone2_code/PD by Josh/PD by Josh.ino
@@ -0,0 +1,36 @@
+#include <Wire.h>
+#include <ZumoShield.h>
+
+ZumoMotors motors;
+ZumoReflectanceSensorArray linesensors(QTR_NO_EMITTER_PIN);
+
+void setup() {
+  int i;
+  int spin_direction = 1;
+  motors.setSpeeds(80*spin_direction, -80*spin_direction);
+  for(i = 0; i<100; i++){
+    linesensors.calibrate();
+    if(i%50 == 25){ // every 50 loops, starting on loop 25...
+      spin_direction = -spin_direction;
+      motors.setSpeeds(80*spin_direction, -80*spin_direction);
+    }
+    delay(20);
+  }
+  motors.setSpeeds(0,0);
+  delay(500);
+}
+
+unsigned int sensor_vals[6];
+int BASE_SPEED = 200;
+double PROPORTION_GAIN = 0.2;
+double DERIVATIVE_GAIN = 3;
+int last_error = 0;
+void loop() {
+  int line_position = linesensors.readLine(sensor_vals);
+  int error = line_position - 2500;
+  int error_change = error - last_error;
+  int left_speed = BASE_SPEED + PROPORTION_GAIN * error + DERIVATIVE_GAIN * error_change;
+  int right_speed = BASE_SPEED + -PROPORTION_GAIN * error + -DERIVATIVE_GAIN * error_change;
+  last_error = error;
+  motors.setSpeeds(left_speed, right_speed);
+}
---
+++ Milestone 2/milestone2_code/milestone2_code2/milestone2_code2.ino
@@ -0,0 +1,101 @@
+#include <ZumoShield.h>
+
+ZumoMotors motors;
+ZumoReflectanceSensorArray sensors;
+Pushbutton button(ZUMO_BUTTON);
+ZumoBuzzer buzzer;
+
+unsigned int sensorValues[6];
+unsigned int linePos; // position of the line (readLine function)
+int error;
+//int lastError = 0;
+//int errorChange;
+//int lastErrorChange = ;
+
+int NORMAL_SPEED = 200; // speed: 0 (stop) - 400 (full)
+int MAX_SPEED = 300;
+int v; // CURRENT SPEED
+int MAX_LINEPOS = 5000;
+int MIDDLE_LINEPOS = MAX_LINEPOS / 2;
+int LEFT_SPEED;
+int RIGHT_SPEED;
+
+int LINEPOS_SPEED_RATIO = (MAX_LINEPOS - MIDDLE_LINEPOS) / (MAX_SPEED - NORMAL_SPEED);
+int ERROR_THRESHOLD = 1000;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  buzzer.play(">g32>>c32");
+
+  // Initialize the sensors
+  sensors.init();
+
+  // calibration by spinning
+  delay(1000);
+  int CALIBRATION_SPEED = 100;
+  for (int t = 0; t < 80; t++) {
+    // 0-10s turn clockwise; 10-30s turn anti-clockwise; 30-50s turn clockwise; 50-70s turn anti-clockwise; 70-80 turn clockwise back to front
+    if ((t <= 10) || (t > 30 && t <= 50) || (t > 70)) {
+      motors.setSpeeds(CALIBRATION_SPEED, -CALIBRATION_SPEED);
+    }
+    else {
+      motors.setSpeeds(-CALIBRATION_SPEED, CALIBRATION_SPEED);
+    }
+    sensors.calibrate();
+    delay(20);
+  }
+
+  //  // calibration by hand
+  //  for (int t =  0; t<100; t++){
+  //    sensors.calibrate();
+  //  }
+
+  buzzer.play(">g32>>c32");
+  motors.setSpeeds(0, 0);
+  // Wait for the user button to be pressed and released
+  button.waitForButton();
+  buzzer.play("L16 cdegreg4");
+  while (buzzer.isPlaying());
+}
+
+void loop() {
+
+  linePos = sensors.readLine(sensorValues);
+  // 0 - directly below sensor 0
+  // 1 - directly below sensor 1
+  // 5 - 5000
+
+  error = linePos - 2500;
+  //errorChange = error-lastError;
+  v = error / LINEPOS_SPEED_RATIO + NORMAL_SPEED;
+  Serial.print("error = ");
+  Serial.print(error);
+  Serial.print("; v = ");
+  Serial.println(v);
+
+  if (abs(error) == 2500) {
+    LEFT_SPEED = MAX_SPEED;
+    RIGHT_SPEED = -MAX_SPEED;
+  }
+  else if (abs(error) > ERROR_THRESHOLD) {
+    if (linePos > 2500) {
+      // right, anti-clockwise
+      LEFT_SPEED = v;
+      RIGHT_SPEED = 0;
+    }
+    else if (linePos < 2500) {
+      // left, clockwise
+      LEFT_SPEED = 0;
+      RIGHT_SPEED = v;
+    }
+  }
+  else {
+    LEFT_SPEED = v;
+    RIGHT_SPEED = v;
+  }
+
+  motors.setSpeeds(LEFT_SPEED, RIGHT_SPEED);
+  //  lastError = error;
+  //  lastErrorChange =  errorChange;
+}
